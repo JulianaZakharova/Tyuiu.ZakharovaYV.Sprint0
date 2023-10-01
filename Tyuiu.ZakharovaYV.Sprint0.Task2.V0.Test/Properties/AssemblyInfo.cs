cf35ba6409bf84c832bae95f521b40a2be94c3c9
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-01 14:52:55 +0500

    Task2

---
+++ Tyuiu.ZakharovaYV.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZakharovaYV.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZakharovaYV.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b760e17-6a8a-4202-b84b-b78962c3204e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZakharovaYV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZakharovaYV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ZakharovaYV.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessage 
+            // из библиотеки Tyuiu.ZakharovaYV.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Юля"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
